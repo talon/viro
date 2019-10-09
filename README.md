a75d734837663ec64f65f8cef48af207694cabf2
talon.computer <hire@talon.computer>
2019-10-09 12:27:45 -0400

    update env.sh
    stupid exec bash tho

--- README.md
+++ README.md
@@ -13,3 +13,26 @@ git clone https://github.com/talon/viro ~/.viro/
 ~/.viro/viro install
 ```
 > put this repo somewhere and then use [`./viro`](./viro) `install` to strap in
+
+# backerpacker's guide to POSIX
+> WIP
+
+## input
+- read
+- fzf
+
+## read
+- tail
+- head
+- bat
+
+## write
+- printf
+- sed
+
+## parse
+- ag
+- awk
+
+# UI
+- tput
--- src/env.sh
+++ src/env.sh
@@ -1,23 +1,38 @@
 source "$VIRO_HOME/src/utils.sh"
 
-VIRO_ENV="$VIRO_HOME/user/ENV"
+VIRO_USER="${VIRO_USER:-$VIRO_HOME/user}"
+VIRO_ENV="${VIRO_ENV:-$VIRO_USER/ENV}"
 
 [[ -e "$(dirname "$VIRO_ENV")" ]] && mkdir -p "$(dirname "$VIRO_ENV")"
 case "$1" in
-  set)
-    name="${2:-"$(viro env | choose "viro env set" | awk '{print $1}')"}"
-    [[ -n "$2" ]] && viro env has "$name" && ! yorn "replace ${name^^}?" "$YES" && exit 1
+  # import)
+  #   rc="${2:-$HOME/.bashrc}"
+  #   echo "$rc"
+  #   grep -vE "(VIRO_HOME|PATH)" "$rc" | grep -E "export" | while read -r env; do
+  #     name="$(echo "$env" | sed 's/=/ /' | awk '{print $2}')"
+  #     value="$(echo "$env" | sed "s/export .*=//")"
+  #     echo "$name"
+  #   done
+  #   ;;
 
-    value="${@:3}"
+  set)
+    name="$2"
+    name="${name:-"$(viro env | fzf --reverse --prompt "viro env set" | awk '{print $1}')"}"
+    [[ -n "$name" ]] && viro env has "${name^^}" && ! yorn "replace ${name^^}?" "$YES" && exit 1
+
+    value="${*:3}"
     value="${value:-"$(prompt "viro env set ${name^^}")"}"
     [[ -z "$value" ]] && exit 1
 
     [[ -f "$VIRO_ENV" ]] && sed -i "/export ${name^^}=/d" "$VIRO_ENV"
     echo "export ${name^^}=\"$value\"" >> "$VIRO_ENV"
-    echo "$value"
+    # FIXME: if there's anyway to apply the ENV without replacing the current shell
+    #        that would be dope
+    exec bash
     ;;
+
   get)
-    name="${2:-"$(viro env | choose "viro env get" | awk '{print $1}')"}"
+    name="${2:-"$(viro env | fzf --reverse --prompt "viro env get" | awk '{print $1}')"}"
     value="$(printenv "${name^^}")"
     if [[ -n "$value" ]]; then
       echo "$value"
@@ -25,32 +40,34 @@ case "$1" in
       viro env set "$name" "$3"
     fi
     ;;
+
   rm)
     names="${*:2}"
-    names="${names:-"$(sed -e 's/export //' -e 's/=/~/' -e "s/['\"]//g" < "$VIRO_ENV" | column -t -s~ | choose "viro env rm" | awk '{print $1}')"}"
+    names="${names:-"$(
+      sed -e 's/export //' -e 's/=/~/' -e "s/['\"]//g" < "$VIRO_ENV" \
+        | column -t -s~ \
+        | fzf --multi --reverse --prompt "viro env rm" \
+        | awk '{print $1}'
+    )"}"
     for name in $names; do
       name="${name^^}"
       if yorn "viro env rm $name?" "$YES"; then
         sed -i "/export $name=/d" "$VIRO_ENV"
-        unset "$name"
       fi
     done
+    # FIXME: if there's anyway to apply the ENV without replacing the current shell
+    #        that would be dope
+    exec bash
     ;;
+
   has) [[ -n "$(printenv "$2")" ]];;
-  import)
-    grep -vE "(VIRO_HOME|PATH)" "$HOME/.bashrc" | grep -E "export" | while read -r env; do
-      name="$(echo "$env" | sed 's/=/ /' | awk '{print $2}')"
-      value="$(echo "$env" | sed "s/export .*=//")"
-      YES="$YES" viro env set "$name" "$value" \
-        && sed -i "/export $name/d" "$HOME/.bashrc"
-    done
-    ;;
-  edit) "$VISUAL" "$VIRO_ENV" && refresh;;
-  choose) viro env | choose "viro env";;
+
+  edit) "$VISUAL" "$VIRO_ENV" && exec bash;;
+
   *) printenv \
-      | sed -e 's/export //' -e 's/=/~/' -e "s/['\"]//g" \
+      | sed -e 's/export //' -e 's/=/|/' -e "s/['\"]//g" \
       | grep -vE "^_" \
       | grep -v "PATH" \
-      | column -t -s~
+      | column -t -s\|
       ;;
 esac
