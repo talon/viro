882a630d26a13ff9df989212cfdda4150bbd4a25
talon.computer <hire@talon.computer>
2019-10-09 16:42:19 -0400

    manpage

--- src/core.sh
+++ src/core.sh
@@ -1,6 +1,8 @@
 export PATH="$PATH:$VIRO_HOME"
-export VIRO_USER="${VIRO_USER:-$VIRO_HOME/user}"
+MANPATH="$MANPATH:$VIRO_HOME"
 
 for SOURCE in "$VIRO_USER/ENV" "$VIRO_USER/PATH" $VIRO_USER/functions/*; do
   [[ -f "$SOURCE" ]] && source "$SOURCE"
 done
+
+export VIRO_USER="${VIRO_USER:-$HOME/.viro}"
